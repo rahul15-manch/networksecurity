3476a23a995f951f8b65dc7f7d793e9bb41b8222
rahul15-manch <rahulmanchanda015@gmail.com>
2025-08-28 01:07:19 +0530

    Model Training and hyperparameter tunning done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_validation import DataValidation,DataValidationConfig
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
 from networksecurity.logging.logger import logging
 import sys
@@ -30,6 +32,14 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print( data_transformation_artifact)
         logging.info(" Data Transformation Completed ")
+
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config ,data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training Completed")
+
+
 
     except Exception as e:
         raise NetworkSecurityException(e, sys) 
